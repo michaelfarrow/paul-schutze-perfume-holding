f008efbc8b38a380f444382f8aeed92e80ac1c3a
Mike Farrow <contact@mikefarrow.co.uk>
2015-10-17 21:07:49 +0100

    fix for compass crashing gulp watch

--- build_tools/gulpfile.js
+++ build_tools/gulpfile.js
@@ -1,10 +1,10 @@
 
 var modernizrTests = [
-	'cssclasses',
-	'rgba',
-	'svg',
-	'boxshadow',
-	'backgroundsize',
+    'cssclasses',
+    'rgba',
+    'svg',
+    'boxshadow',
+    'backgroundsize',
 ];
 
 var gulp       = require('gulp'),
@@ -21,21 +21,26 @@ var gulp       = require('gulp'),
     del        = require('del'),
     favicons   = require('gulp-favicons'),
     paths      = require('path');
+    gutil      = require('gulp-util');
 
+
+function handleError(err) {
+    this.emit('end');
+}
 
 gulp.task('del-generated-favicon-html', function () {
     return del([
-    	path.includes().append('favicons.html').s()
+        path.includes().append('favicons.html').s()
     ], {
-    	force: true
+        force: true
     });
 });
 
 gulp.task('del-temp-favicon-source', function () {
     return del([
-    	path.includes().append('._favicons.html').s()
+        path.includes().append('._favicons.html').s()
     ], {
-    	force: true
+        force: true
     });
 });
 
@@ -45,9 +50,9 @@ gulp.task('generate-favicons', ['del-generated-favicon-html'], function () {
         .pipe(gulp.dest(path.includes().s()))
         .pipe(favicons({
             files: {
-            	dest: '../' + path.images().append('favicons').s(),
-            	html: path.includes().append('favicons.html').s(),
-            	iconsPath: '/_assets/img/favicons/'
+                dest: '../' + path.images().append('favicons').s(),
+                html: path.includes().append('favicons.html').s(),
+                iconsPath: '/_assets/img/favicons/'
             },
             settings: { background: '#FFFFFF' }
         }));
@@ -55,161 +60,162 @@ gulp.task('generate-favicons', ['del-generated-favicon-html'], function () {
 
 gulp.task('create-favicons', ['generate-favicons'], function () {
     return del([
-    	path.includes().append('._favicons.html').s()
+        path.includes().append('._favicons.html').s()
     ], {
-    	force: true
+        force: true
     });
 });
 
 gulp.task('copy-bootstrap-fonts', function() {
-	return gulp.src(path.bower('bootstrap-sass-official/assets/fonts/bootstrap/*').s())
-		.pipe(gulp.dest(path.fonts().vendor().append('bootstrap').s()))
-		.pipe(size({ title:'Bootstrap Fonts', gzip:true }));
+    return gulp.src(path.bower('bootstrap-sass-official/assets/fonts/bootstrap/*').s())
+        .pipe(gulp.dest(path.fonts().vendor().append('bootstrap').s()))
+        .pipe(size({ title:'Bootstrap Fonts', gzip:true }));
 });
 
 gulp.task('copy-pygments-css', function() {
-	return gulp.src(path.bower('pygments/css/*').s())
-		.pipe(minify())
-		.pipe(gulp.dest(path.css().vendor().append('pygments').s()))
-		.pipe(size({ title:'Pygments CSS', gzip:true }));
+    return gulp.src(path.bower('pygments/css/*').s())
+        .pipe(minify())
+        .pipe(gulp.dest(path.css().vendor().append('pygments').s()))
+        .pipe(size({ title:'Pygments CSS', gzip:true }));
 });
 
 gulp.task('compile-compass', function() {
-	return gulp.src(path.sass().append('*.scss').s())
-		.pipe(compass({
-			css: paths.join(__dirname, path.css().s()),
-			sass: paths.join(__dirname, path.sass().s()),
-			image: paths.join(__dirname, path.images().s()),
-		}))
-		.pipe(minify())
-		.pipe(gulp.dest(path.css().s()));
+    return gulp.src(path.sass().append('*.scss').s())
+        .pipe(compass({
+            css: paths.join(__dirname, path.css().s()),
+            sass: paths.join(__dirname, path.sass().s()),
+            image: paths.join(__dirname, path.images().s()),
+        }))
+        .on('error', handleError)
+        .pipe(minify())
+        .pipe(gulp.dest(path.css().s()));
 });
 
 gulp.task('custom-modernizr', function() {
 
-	return gulp.src(path.bower('modernizr/modernizr.js').s())
-		.pipe(size({ title:'Original', showFiles:true }))
-		.pipe(modernizr(modernizrTests))
-		.pipe(addSrc.append([
-			path.js().append('modernizr/*.js').s()
-		]))
-		.pipe(concat('modernizr.js'))
-		.pipe(gulp.dest(path.js().vendor().s()))
-		.pipe(uglify())
-		.pipe(gulp.dest(path.js().vendor().s()))
-		.pipe(size({ title:'Optimized', showFiles:true, gzip:true }));
+    return gulp.src(path.bower('modernizr/modernizr.js').s())
+        .pipe(size({ title:'Original', showFiles:true }))
+        .pipe(modernizr(modernizrTests))
+        .pipe(addSrc.append([
+            path.js().append('modernizr/*.js').s()
+        ]))
+        .pipe(concat('modernizr.js'))
+        .pipe(gulp.dest(path.js().vendor().s()))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.js().vendor().s()))
+        .pipe(size({ title:'Optimized', showFiles:true, gzip:true }));
 });
 
 gulp.task('lint-js', function() {
-	return gulp.src([
-			path.js().append('/**/*.js').s(),
-			'!' + path.js().vendor().append('*.js').s()
-		])
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshint_s))
-		.pipe(jshint.reporter('fail'));
+    return gulp.src([
+            path.js().append('/**/*.js').s(),
+            '!' + path.js().vendor().append('*.js').s()
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter(jshint_s))
+        .pipe(jshint.reporter('fail'));
 });
 
 
 gulp.task('build', [
-	'create-favicons',
-	'copy-bootstrap-fonts',
-	'copy-pygments-css',
-	'custom-modernizr',
-	'compile-compass',
-	'lint-js',
+    'create-favicons',
+    'copy-bootstrap-fonts',
+    'copy-pygments-css',
+    'custom-modernizr',
+    'compile-compass',
+    'lint-js',
 ]);
 
 gulp.task('default', [
-	'build',
+    'build',
 ]);
 
 gulp.task('watch', function() {
-	gulp.watch(path.sass().append('*.scss').s(), ['compile-compass']);
-	gulp.watch([
-		path.js().append('/**/*.js').s(),
-		'!' + path.js().vendor().append('*.js').s()
-	], ['lint-js']);
-	gulp.watch(path.js().append('modernizr/*.js').s(), ['custom-modernizr']);
+    gulp.watch(path.sass().append('*.scss').s(), ['compile-compass']);
+    gulp.watch([
+        path.js().append('/**/*.js').s(),
+        '!' + path.js().vendor().append('*.js').s()
+    ], ['lint-js']);
+    gulp.watch(path.js().append('modernizr/*.js').s(), ['custom-modernizr']);
 });
 
 
 function path(p, f){
-	if(f) f = '/' + f;
-	this.path = p + path.format(f);
+    if(f) f = '/' + f;
+    this.path = p + path.format(f);
 };
 
 path.base = function() {
-	return new path('../_site');
+    return new path('../_site');
 };
 
 path.layouts = function() {
-	return path.base().append('_layouts');
+    return path.base().append('_layouts');
 };
 
 path.includes = function() {
-	return path.base().append('_includes');
+    return path.base().append('_includes');
 };
 
 path.assets = function() {
-	return path.base().append('_assets');
+    return path.base().append('_assets');
 };
 
 path.bower = function(p) {
-	return new path('libs').append(p);
+    return new path('libs').append(p);
 };
 
 path.sass = function() {
-	return path.assets().append('scss');
+    return path.assets().append('scss');
 };
 
 path.css = function() {
-	return path.assets().append('css');
+    return path.assets().append('css');
 };
 
 path.images = function() {
-	return path.assets().append('img');
+    return path.assets().append('img');
 };
 
 path.fonts = function() {
-	return path.assets().append('fonts');
+    return path.assets().append('fonts');
 };
 
 path.js = function() {
-	return path.assets().append('js');
+    return path.assets().append('js');
 };
 
 path.format = function(p) {
-	if(!p) return '';
-	return p;
+    if(!p) return '';
+    return p;
 },
 
 path.prototype = {
 
-	path: '',
+    path: '',
 
-	toString: function(){
-		return this.path;
-	},
+    toString: function(){
+        return this.path;
+    },
 
-	s: function(){
-		return this.toString();
-	},
+    s: function(){
+        return this.toString();
+    },
 
-	append: function(p) {
-		return new path(this.path, p);
-	},
+    append: function(p) {
+        return new path(this.path, p);
+    },
 
-	compiled: function() {
-		return this.append('.compiled');
-	},
+    compiled: function() {
+        return this.append('.compiled');
+    },
 
-	minified: function() {
-		return this.append('.minified');
-	},
+    minified: function() {
+        return this.append('.minified');
+    },
 
-	vendor: function() {
-		return this.append('vendor');
-	}
+    vendor: function() {
+        return this.append('vendor');
+    }
 
 }
